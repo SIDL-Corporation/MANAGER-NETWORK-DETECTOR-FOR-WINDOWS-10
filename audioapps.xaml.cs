d00153ea7a2aeaa81b82597a038f5603df8473d7
SIDL CORPORATION <aymeric.sav.218@outlook.com>
2021-03-26 14:06:24 +0100

    OFFICIEL CODE SOURCE
    Code source officiel de la version "10.6.1.0".

---
+++ audioapps.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MANAGER_NETWORK_DETECTOR_
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class audioapps : Page
+    {
+        public audioapps()
+        {
+            this.InitializeComponent();
+            // Set theme for window root
+            FrameworkElement root = (FrameworkElement)Window.Current.Content;
+            root.RequestedTheme = AppSettings.Theme;
+            CustomizeTitleBar();
+            ElementSoundPlayer.State = ElementSoundPlayerState.On;
+            CurrentVol.Value = ElementSoundPlayer.Volume * 100;
+            if (ElementSoundPlayer.State == ElementSoundPlayerState.On)
+                soundToggle.IsOn = true;
+            if (ElementSoundPlayer.SpatialAudioMode == ElementSpatialAudioMode.On && soundToggle.IsOn == true)
+                spatialAudioBox.IsChecked = true;
+        }
+
+        private void CustomizeTitleBar()
+        {
+            // Customisation de la bar de titre
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(customTitleBar);
+        }
+
+        private void spatialAudioBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (soundToggle.IsOn == true)
+            {
+                ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.On;
+            }
+        }
+
+        private void spatialAudioBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (soundToggle.IsOn == true)
+            {
+                ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.Off;
+            }
+        }
+
+        private void soundToggle_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (soundToggle.IsOn == true)
+            {
+                spatialAudioBox.IsEnabled = true;
+                ElementSoundPlayer.State = ElementSoundPlayerState.On;
+            }
+            else
+            {
+                spatialAudioBox.IsEnabled = false;
+                spatialAudioBox.IsChecked = false;
+
+                ElementSoundPlayer.State = ElementSoundPlayerState.Off;
+                ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.Off;
+            }
+        }
+
+        private void BUTTON_BACK_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(HomeSettings), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromLeft });
+        }
+
+        private void SliderVolum_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            string msg = String.Format("{0} %", e.NewValue);
+            this.VolumTXT.Text = msg;
+
+            Slider slider = sender as Slider;
+            double volumeLevel = slider.Value / 100;
+            ElementSoundPlayer.Volume = volumeLevel;
+        }
+    }
+}
